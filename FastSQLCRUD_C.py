3089848aa7c91027aacbc6f5fc67310be5a77b7e
aloHyomora <alohomoratolit@naver.com>
2025-04-11 00:44:03 +0900

    [Feat] : MySQL CRUD Update

--- FastSQLCRUD_C.py
+++ FastSQLCRUD_C.py
@@ -1,4 +1,4 @@
-import email
+from typing import Optional
 from FastSQLStart import DATABASE_URL
 from fastapi import FastAPI, Depends
 from sqlalchemy import create_engine, Column, Integer, String
@@ -46,6 +46,7 @@ def read_root():
 
 # curl -X POST "http://127.0.0.1:8000/users/" -H "Content-Type: application/json" -d "{\"username\":\"KING\",\"email\":\"King@example.com\"}"
 # curl -X 'GET' "http://127.0.0.1:8000/users/2" -H "accept: application/json"
+# curl -X PUT "http://127.0.0.1:8000/users/1" -H "Content-Type: application/json" -d "{\"username\":\"newName\",\"email\":\"NEW@example.com\"}"
 # 사용자를 생성하는 POST API 엔드포인트를 추가합니다.
 @app.post("/users/")
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
@@ -57,6 +58,7 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
 
     return {"id": new_user.id, "username": new_user.username, "email": new_user.email}    
 
+# 사용자를 조회하는 GET API 엔드포인트를 추가합니다.
 @app.get("/users/{user_id}")
 def read_user(user_id: int, db: Session = Depends(get_db)):
     # 주어진 ID를 가진 사용자를 DB에서 조회합니다.
@@ -65,3 +67,23 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
     if user is None:
         return {"error": "User not found"}
     return {"id": user.id, "username": user.username, "email": user.email}
+
+class UserUpdate(BaseModel):
+    username: Optional[str] = None
+    email: Optional[str] = None
+
+# Update API 엔드포인트를 추가합니다.
+@app.put("/users/{user_id}")
+def update_user(user_id: int, user: UserUpdate, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if db_user is None:
+        return {"error": "User not found"}
+    
+    if user.username is not None:
+        db_user.username = user.username
+    if user.email is not None:
+        db_user.email = user.email
+
+    db.commit()
+    db.refresh(db_user)
+    return {"id": db_user.id, "username": db_user.username, "email": db_user.email}
